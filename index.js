45a7d8b6b27352f1e8b7c496db40848ac481c80e
Jody Hestick <jhestick@gmail.com>
2018-04-04 23:04:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function takeANumber(katzDeliLine, name){
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`
 }
 
-function nowServing(){
-  if()
+function nowServing(line){
+  if(line.length === 0){
+    return ""
+  }
 }
