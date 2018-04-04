94addf800358d4f1e0d714382fae07d7697ea591
Jody Hestick <jhestick@gmail.com>
2018-04-04 23:26:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function nowServing(line){
 
 function currentLine(line){
   if(line.length === 0){
-    return "The line is currently empty"
+    return "The line is currently empty."
   }else{
     var lineList = "The line is currently:"
     for(var i=1; i<=line.length; i++){
